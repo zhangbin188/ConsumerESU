0945634ea781f407c96015506db83daacc460319
abbodi1406 <33669284+abbodi1406@users.noreply.github.com>
2025-08-30 02:38:53 +0300

    Update batch script to accept parameters

--- Consumer_ESU_Enrollment_run.cmd
+++ Consumer_ESU_Enrollment_run.cmd
@@ -2,4 +2,4 @@
 set "_PSf=%~dp0Consumer_ESU_Enrollment.ps1"
 setlocal EnableDelayedExpansion
 set "_PSf=!_PSf:'=''!"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^& "'!_PSf!'"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^& "'!_PSf!' %*"
